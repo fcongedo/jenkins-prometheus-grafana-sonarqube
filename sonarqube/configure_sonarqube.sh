7e528c9f614561d02e5f4ff15403b6fcba8159ae
Nicolás Batistelli <135087264+NicolasBatistelli@users.noreply.github.com>
2025-01-07 15:21:58 -0300

    Update configure_sonarqube.sh

--- sonarqube/configure_sonarqube.sh
+++ sonarqube/configure_sonarqube.sh
@@ -3,7 +3,7 @@
 SONAR_URL="http://localhost:9000"
 OLD_PASSWORD="admin"
 NEW_PASSWORD="administrator"
-TOKEN_NAME="jenkins-token"
+#TOKEN_NAME="jenkins-token"
 USERNAME="admin"
 
 echo "Esperando a que SonarQube esté disponible en $SONAR_URL ..."
@@ -18,13 +18,3 @@ echo "SonarQube está disponible. Procediendo con la configuración."
 curl -X POST -u "$USERNAME:$OLD_PASSWORD" \
   "$SONAR_URL/api/users/change_password" \
   -d "login=$USERNAME&password=$NEW_PASSWORD&previousPassword=$OLD_PASSWORD"
-
-# Crear un token de autenticación
-TOKEN=$(curl -X POST -u "$USERNAME:$NEW_PASSWORD" \
-  "$SONAR_URL/api/user_tokens/generate" \
-  -d "name=$TOKEN_NAME" | jq -r '.token')
-
-echo "Token generado: $TOKEN"
-
-# Guardar el token para su uso posterior
-echo "$TOKEN" | sudo tee /opt/sonarqube/conf/sonarqube_token > /dev/null
